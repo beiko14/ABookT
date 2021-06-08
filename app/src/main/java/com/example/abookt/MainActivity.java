fa04ef2e6657009a7aba377872fd5ee1ecd6a963
Beiko14 <heiko.hollerith14@gmx.de>
2021-06-08 08:06:50 +0200

    implementing MaterialDesign. Creating MainActivity-Layout and Design. Initiating MainActivity-Buttons

--- app/src/main/java/com/example/abookt/MainActivity.java
+++ app/src/main/java/com/example/abookt/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.abookt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAllBooks, buttonCurrentlyReading,
+            buttonAllreadyRead, buttonWantToReadBooks, buttonFavorite, buttonAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        buttonAllBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllBooksActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    public void initViews(){
+        buttonAllBooks = findViewById(R.id.buttonAllBooks);
+        buttonCurrentlyReading = findViewById(R.id.buttonCurrentlyReading);
+        buttonAllreadyRead = findViewById(R.id.buttonAllreadyRead);
+        buttonWantToReadBooks = findViewById(R.id.buttonWantToReadBooks);
+        buttonFavorite = findViewById(R.id.buttonFavorite);
+        buttonAbout = findViewById(R.id.buttonAbout);
+    }
+
 }
